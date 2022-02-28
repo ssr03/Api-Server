545966f90ad61ec99d7cea5a4973c07567b5b70c
390815 <sora.son@iljin.co.kr>
2022-02-28 17:11:53 +0900

    2022.2.28 schedule initial

---
+++ src/main/java/framework/apiserver/core/schedule/ScheduleServiceImpl.java
@@ -0,0 +1,16 @@
+package framework.apiserver.core.schedule;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService{
+    @Override
+    public void runMailing() {
+
+    }
+
+    @Override
+    public void runDataCollection() {
+
+    }
+}
